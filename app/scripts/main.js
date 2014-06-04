e8c93ed19f7fb93ca0925a36a1114d9f1c018206
João Netto <hi@joaonetto.me>
2014-06-04 13:58:05 -0300

    New layout implementation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 $(function() {
-    $('.img-responsive').on('click', function () {
-        $('#image').html('<img class="img-responsive" src="'+$(this).attr('src')+'">');
-        $('#image img').css('height', $(window).innerHeight() - 80);
-    });
+    // $('.img-responsive').on('click', function () {
+    //     $('#image').html('<img class="img-responsive" src="'+$(this).attr('src')+'">');
+    //     $('#image img').css('height', $(window).innerHeight() - 80);
+    // });
 
-    $(window).on('resize load', function () {
-        $('.follow').css('height', $(window).innerHeight());
-    });
+    // $(window).on('resize load', function () {
+    //     $('.follow').css('height', $(window).innerHeight());
+    // });
 });
