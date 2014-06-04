1af59c84e31dd70d3234ba4f825b2f300f2449bf
João Netto <hi@joaonetto.me>
2014-06-04 13:00:44 -0300

    Navigation changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,5 +2,10 @@
 $(function() {
     $('.img-responsive').on('click', function () {
         $('#image').html('<img class="img-responsive" src="'+$(this).attr('src')+'">');
+        $('#image img').css('height', $(window).innerHeight() - 80);
+    });
+
+    $(window).on('resize load', function () {
+        $('.follow').css('height', $(window).innerHeight());
     });
 });
